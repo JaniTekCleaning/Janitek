d67797d00088dc2753c416669c3603824d970d92
Ronald Lugge <ronlugge@gmail.com>
2017-04-10 09:33:25 -0700

    Removed 'from' value on emails to comply with email vendor's policies.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,7 +5,7 @@ class ContactMailer < ActionMailer::Base
     @contact=contact
     @member=contact.member
     target=@member.client.staff.nil? ? ENV["CONTACT_EMAIL"] : @member.client.staff.email
-    mail(to:target, subject:"Message From: "+contact.member.full_name, from:contact.member.email)
+    mail(to:target, subject:"Message From: "+contact.member.full_name)
   end
 
   def service_request(fields, member)
@@ -13,6 +13,6 @@ class ContactMailer < ActionMailer::Base
     @member=member
     @service_request=ServiceRequest.first
     target=@member.client.staff.nil? ? ENV["CONTACT_EMAIL"] : @member.client.staff.email
-    mail(to:target, subject:"Service Request From: "+member.full_name, from:member.email)
+    mail(to:target, subject:"Service Request From: "+member.full_name)
   end
 end
